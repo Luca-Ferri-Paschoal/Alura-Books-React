67af961e6614e57828e9a5e3407ab975f247ecfa
Luca-Ferri <lucaferrip@gmail.com>
2022-11-02 14:56:07 -0300

    Criando primeiro componente com Styled-Components, no caso, um AppContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
 import Cabecalho from './components/Cabecalho';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-image: linear-gradient(
+    90deg, 
+    #002F52 35%, 
+    #326589
+  );
+`;
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer >
       <Cabecalho />
-    </div>
+    </AppContainer>
   );
 }
 
